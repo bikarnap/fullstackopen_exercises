b908ed66c8049486abe9b925fe1db7f120787048
Bikarna Pokharel <bikarnap@gmail.com>
2023-01-11 06:11:56 +0200

    Modify to dispaly statistics only when a feedback is provided

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,18 +15,24 @@ const Statistics = ({ good, neutral, bad }) => (
 const App = () => {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
-  const [neutral, setNeutral] = useState(0)
+  const [neutral, setNeutral] = useState(0);
+
+  const showStatistics = () => {
+    if (good === 0 && neutral === 0 && bad === 0)
+      return <p>No feedback received yet, provide feedback using the buttons</p>
+    return <Statistics
+      good={good}
+      neutral={neutral}
+      bad={bad}
+    />
+  }
   return (
     <div>
       <h2>give feedback</h2>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-      />
+      {showStatistics()}
     </div>
   );
 }
