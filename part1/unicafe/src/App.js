bc796c80fcbc76e29dac802d734ae9df0e7b1938
Bikarna Pokharel <bikarnap@gmail.com>
2023-01-11 05:59:28 +0200

    Add more statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,6 +14,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral} </div>
       <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
+      <div>positive {good/(good + neutral + bad) * 100} %</div>
     </div>
   );
 }
