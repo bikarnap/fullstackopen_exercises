fb3e8774aca0b100a513987713557100ddfed3ab
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-11 13:58:19 +0200

    Refactor to create separate components

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useState } from "react"
 
-const Statistics = ({ good, neutral, bad }) => (
-  <div>
-    <h2>statistics</h2>
-    <div>good {good}</div>
-    <div>neutral {neutral} </div>
-    <div>bad {bad}</div>
-    <div>all {good + neutral + bad}</div>
-    <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
-    <div>positive {good/(good + neutral + bad) * 100} %</div>
-  </div>
-);
+const StatisticLine = ({ statistic, value, unit='' }) => <div>{statistic} {value} {unit}</div>
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / (good + neutral + bad);
+  const positive = good/(good + neutral + bad) * 100;
+  return (
+    <div>
+      <h2>statistics</h2>
+      <StatisticLine statistic="good" value={good} />
+      <StatisticLine statistic="neutral" value={neutral} />
+      <StatisticLine statistic="bad" value={bad} />
+      <StatisticLine statistic="all" value={all} />
+      <StatisticLine statistic="average" value={average} />
+      <StatisticLine statistic="positive" value={positive} unit="%" />
+    </div>
+  );
+}
+
+const Button = ({ handleClick, label }) => <button onClick={handleClick}>{label}</button>
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -29,9 +38,9 @@ const App = () => {
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => setGood(good + 1)} label="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} label="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} label="bad" />
       {showStatistics()}
     </div>
   );
