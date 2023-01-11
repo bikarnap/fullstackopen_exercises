324dd994d59c948473774dba4030563e9ab32c6a
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-11 15:00:31 +0200

    Add functionality to generate a random number
    and display a random anecdote from the given array

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const Button = ({ handleClick, label }) => <button onClick={handleClick}>{label}</button>;
 
 function App() {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0);
+
+  const generateSelection = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    console.log(randomNumber)
+    setSelected(randomNumber);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {anecdotes[selected]}
+     <div>
+      <Button handleClick={generateSelection} label="next anecdote" />
+     </div>
     </div>
   );
 }
