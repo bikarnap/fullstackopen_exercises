ba60e9b6fb31f8a019e10a6fd7d26692fe6d1b00
Bikarna Pokharel <bikarnap@gmail.com>
2023-01-11 06:04:09 +0200

    Refactor to create a separate component
    Component renders the satistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,17 @@
 import { useState } from "react";
 
+const Statistics = ({ good, neutral, bad }) => (
+  <div>
+    <h2>statistics</h2>
+    <div>good {good}</div>
+    <div>neutral {neutral} </div>
+    <div>bad {bad}</div>
+    <div>all {good + neutral + bad}</div>
+    <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
+    <div>positive {good/(good + neutral + bad) * 100} %</div>
+  </div>
+);
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [bad, setBad] = useState(0);
@@ -10,13 +22,11 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>statistics</h2>
-      <div>good {good}</div>
-      <div>neutral {neutral} </div>
-      <div>bad {bad}</div>
-      <div>all {good + neutral + bad}</div>
-      <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
-      <div>positive {good/(good + neutral + bad) * 100} %</div>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   );
 }
