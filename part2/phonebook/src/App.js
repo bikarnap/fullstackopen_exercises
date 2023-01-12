69d0dfc5aaa7fa4ca5b457e44107af11d6eef6c8
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-12 14:04:50 +0200

    Add feature to add phone number

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,9 +4,11 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Bikarna Pokharel',
+      number: '0443350986'
     }
   ]);
   const [newPerson, setNewPerson] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -17,25 +19,38 @@ const App = () => {
       alert(`${newPerson} has already been added to phonebook!`);
     } else {
       const personObject = {
-        name: newPerson
+        name: newPerson,
+        number: newNumber
       }
       setPersons(persons.concat(personObject));
     }
     setNewPerson('');
+    setNewNumber('');
   };
 
   const handleNewPerson = (event) => setNewPerson(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
 
   return (
     <div>
       <h1>Phonebook</h1>
       <form onSubmit={addPerson}>
-        name: <input type="text" onChange={handleNewPerson} value={newPerson} />
-        <button type='submit'>add</button>
+        <div>
+          name: <input type="text" onChange={handleNewPerson} value={newPerson} />
+        </div>
+        <div>
+          number: <input type="text" onChange={handleNumberChange} value={newNumber} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
-      
+      {persons.map(person => 
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      )}
     </div>
   );
 }
