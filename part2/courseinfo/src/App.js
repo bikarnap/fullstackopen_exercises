6c67667d8b69feb9a9d93a36a9450fc566b2d06d
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-01-12 12:06:46 +0200

    Import Course module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,35 +1,4 @@
-const Header = (props) => <h2>{props.course}</h2>
-
-const Part = (props) => <p>{props.part} {props.exercises}</p>
-
-const Content = (props) => (
-  <div>
-    {props.parts.map(part =>
-      <Part
-        key={part.id}
-        part={part.name}
-        exercises={part.exercises}
-      />
-    )}
-  </div>
-)
-
-const Total = (props) => {
-  const totalExercises = props.parts.reduce((sum, exercises) => sum + exercises.exercises, 0);
-  return (
-    <p><strong>total of {totalExercises} exercises</strong></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
