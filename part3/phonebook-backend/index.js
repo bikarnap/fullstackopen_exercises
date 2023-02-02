6a465fb9eb0543f7a148b52c4ba73bff30391d0e
Pokharel Bikarna (Ext) <ext.Bikarna.Pokharel@kalmarglobal.com>
2023-02-02 14:47:35 +0200

    Add route to delete a person's info

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-const persons = [
+let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -40,6 +40,12 @@ app.get('/api/persons/:id', (req, res) => {
     res.json(person);
   else
     res.status(404).end();
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
